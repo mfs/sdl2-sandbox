0f9d1d01767361eb3571c261eeb635fe2f3c55c5
Mike Sampson <mike@sambodata.com>
2016-04-14 21:03:31 +1000

    Factor out hash().

--- src/starfield.rs
+++ src/starfield.rs
@@ -5,13 +5,18 @@ use std::f64::consts;
 use fnv::FnvHasher;
 use std::hash::Hasher;
 
+fn hash(value: &str) -> u64 {
+    let mut hash = FnvHasher::default();
+    hash.write(value.as_bytes());
+
+    hash.finish()
+}
+
 fn jitter(a: f64, b: f64, k: f64, m: f64, n: f64 ) -> f64 {
 
     let s = format!("{}:{}:{}", k, m, n);
-    let mut hash = FnvHasher::default();
-    hash.write(s.as_bytes());
 
-    let frac: f64 = hash.finish() as f64 / (::std::u64::MAX >> 1) as f64;
+    let frac: f64 = hash(&s) as f64 / (::std::u64::MAX >> 1) as f64;
 
     a + frac
 }
